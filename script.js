3b5839a41a6d95573d6d9781de1d64422fb9616c
BishalKrShaw <bishalshaw.mfis@gmail.com>
2024-02-11 21:45:15 +0530

    Feature

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const input = document.querySelector("form input[type='text']");
+const button = document.querySelector("form input[type='submit']");
+const message = document.querySelector("#displayMessage");
+const gameContainer = document.querySelector("#game-container");
+let pastGuesses = document.querySelector("p #pastGuesses");
+let pastGuessesArr = [];
+let chanceCounterNumber = document.querySelector("#chanceCount span");
+
 // Generate Random Number between 1 - 100
 const generatedNumber = Math.floor(Math.random()*100) + 1;
 
@@ -6,13 +14,6 @@ const form = document.querySelector("form");
 form.addEventListener("submit", (event)=>{
     event.preventDefault();
 });
-
-const input = document.querySelector("form input[type='text']");
-const button = document.querySelector("form input[type='submit']");
-const message = document.querySelector("#displayMessage");
-let pastGuesses = document.querySelector("p #pastGuesses");
-let pastGuessesArr = [];
-let chanceCounterNumber = document.querySelector("#chanceCount span");
 
 // Convert input from String type to Number type
 function convertInputType() {
@@ -30,6 +31,7 @@ function checkGuessNumber() {
         button.style.background = "grey";
         button.style.cursor = "no-drop";
         input.style.cursor = "no-drop";
+        tryAgain();
     }
     else if(inputNumber > generatedNumber) {
         message.innerHTML = `You guesses it too Large`;
@@ -59,7 +61,15 @@ function chanceCounter() {
         button.style.cursor = "no-drop";
         message.innerHTML = `Your Limit is Over!!`;
         input.style.cursor = "no-drop";
+        tryAgain();
     }
+}
+
+// Try Again Button
+function tryAgain() {
+    const btnDiv = document.createElement("div");
+    btnDiv.innerHTML = `<button id="playAgain" onclick = "window.location.reload(true)">Play Again</button>`;
+    gameContainer.appendChild(btnDiv);
 }
 
 // Work when button got clicked
